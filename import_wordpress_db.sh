46386af8ff8d6a403561fb68833665caa9c88ef8
Osvaldo Gago <opgago@yahoo.com>
2021-10-19 22:14:53 +0200

    Complete the import wordpress script

--- import_wordpress_db.sh
+++ import_wordpress_db.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 rpl https://${1} 'https://localhost' /import-export/${2}
 rpl ${1} 'localhost' /import-export/${2}
+mariadb -u root -e "DROP DATABASE wordpressdb;"
+mariadb -u root -e "CREATE DATABASE wordpressdb;"
+mariadb -u root wordpressdb < /import-export/${2}
 
-# Replaces references to your production domain to localhost in the database.
+# Replaces references to your production domain to localhost in the database and imports the database.
 # Use like: $ ./import_wordpress_db.sh yourdomain.org yoursite_wordpress_db.sql
